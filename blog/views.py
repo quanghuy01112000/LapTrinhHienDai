9c183929c4a6ad0e23bb14502cbc960bdd93a002
Huy Nguyen <huy.nq@bliss-interactive.com>
2022-05-08 22:19:49 +0700

    add category

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Category
+from .models import Post
 
 def index(request):
     return render(request, "pages/home.html")
 
 
 def category(request, slug):
-    return render(request, "pages/category.html", {"slug": slug})
+    id = Category.objects.get(slug = slug).id
+    posts = Post.objects.filter(category_id = id)
+    return render(request, "pages/category.html", 
+                    {"slug": slug,
+                    'posts': posts})
 
 
 def contact(request):
