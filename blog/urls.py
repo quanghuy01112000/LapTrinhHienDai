f36888ca79fffba14037a72af3a7adb8b6a38894
Kiều Đặng Kim Bảo <kieudangkimbao@gmail.com>
2022-05-09 06:48:08 +0700

    Bao dien tu phien ban alpha

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact", views.contact, name="contact"),
+    path("detail/<str:slug>", views.detail, name="detail"),
     path("<str:slug>", views.category, name="category"),
 ]
